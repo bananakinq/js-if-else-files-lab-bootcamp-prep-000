768cf75303ba379a0ead792935aab9e2c3d62d75
Josue Jimenez <josuelps71@gmail.com>
2019-01-26 08:08:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 var name = "Joe"
 var height = "74"
-
-
+if (name === "susan")
+   console.log("the name is correct") 
 // Don't worry about this
 module.exports = { name, height
 }
-
+var = susan
+if (name === "susan") {
+   console.log("the name is correct")
+} else {
+console("expected the name to be susan")
+}
+var name = "susan"
